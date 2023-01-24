72186b73f86d6be45965a184a19f1c38bf2a1d40
Hristo Nestorov <nbglink@gmail.com>
2023-01-24 21:10:41 +0200

    changed Docker file

--- src/main/java/com/devopskills/demoapp/DemoAppApplication.java
+++ src/main/java/com/devopskills/demoapp/DemoAppApplication.java
@@ -3,13 +3,17 @@ package com.devopskills.demoapp;
 import io.dekorate.kubernetes.annotation.KubernetesApplication;
 import io.dekorate.kubernetes.annotation.Label;
 import io.dekorate.kubernetes.annotation.Port;
+import io.dekorate.kubernetes.annotation.ServiceType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 @KubernetesApplication(
 		ports = @Port(name = "http", containerPort = 9090),
-		labels = @Label(key = "version", value = "v1")
+		labels = @Label(key = "version", value = "v1"),
+		serviceType = ServiceType.LoadBalancer
+
 )
 public class DemoAppApplication {
 
